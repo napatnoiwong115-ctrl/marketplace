0552f07bfb2f74b095e651e20664d9d21c515360
napatnoiwong115-ctrl <napatnoiwong115@gmail.com>
2026-09-02 22:18:14 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="th" class="dark">
+    <html lang="th" className="dark">
       <body className={`${kanit.className} min-h-screen antialiased`}>
         <ThemeProvider>
           {children}
